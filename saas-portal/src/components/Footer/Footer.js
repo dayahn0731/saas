8315919218f2ab8ba639982f4a1beffb4570c24d
Akshat Prakash <akshatprakash@Akshats-MacBook-Pro.local>
2021-04-14 22:36:14 -0400

    fixed stuff

--- saas-portal/src/components/Footer/Footer.js
+++ saas-portal/src/components/Footer/Footer.js
@@ -4,24 +4,11 @@ function Footer() {
     return (
         <div className="pt-16">
             <div className="w-full bg-gray-100 py-12">
-                <div className="container mx-auto xl:flex lg:flex text-center xl:text-left lg:text-left">
-                    <div className="xl:w-3/6 lg:w-3/6 sm:w-full text-center xl:text-left mb-6 xl:mb-0 lg:mb-0">
+                <div className="container mx-auto xl:flex lg:flex text-center">
+                    <div className="xl:w-3/6 lg:w-3/6 sm:w-full text-center xl:text-left mb-6 xl:mb-0 lg:mb-0 justify-between">
                         <p className="text-gray-800">{new Date().getFullYear()} SLINGSHOT AHEAD INC. All Rights Reserved</p>
                     </div>
-                    <div className="xl:w-3/6 lg:w-3/6 sm:w-full">
-                        <a href="mailto:info@slingshotmentoring.com" className="mr-0 pr-0">info@slingshotmentoring.com</a>
-                        {/* <ul className="xl:flex lg:flex md:flex sm:flex justify-around">
-                            <li className="text-gray-800 hover:text-gray-900 mb-3 xl:mb-0 lg:mb-0 md:mb-0 sm:mb-0">
-                                <a href="javascript:void(0)">Terms of service</a>
-                            </li>
-                            <li className="text-gray-800 hover:text-gray-900 mb-3 xl:mb-0 lg:mb-0 md:mb-0 sm:mb-0">
-                                <a href="javascript:void(0)">Privacy Policy</a>
-                            </li>
-                            <li className="text-gray-800 hover:text-gray-900 mb-3 xl:mb-0 lg:mb-0 md:mb-0 sm:mb-0">
-                                <a href>Security</a>
-                            </li>
-                        </ul> */}
-                    </div>
+                    <a href="mailto:info@slingshotmentoring.com" className="text-right">info@slingshotmentoring.com</a>
                 </div>
             </div>
         </div>
