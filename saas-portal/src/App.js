f68535887ae086109e27303b60fb497d00cc0955
Akshat Prakash <akshatprakash@Akshats-MacBook-Pro.local>
2021-04-27 07:55:40 -0400

    importing router

--- saas-portal/src/App.js
+++ saas-portal/src/App.js
@@ -1,6 +1,12 @@
 import './App.css';
 import LandingPage from './pages/LandingPage/LandingPage';
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
 function App() {
   return (
     <>
