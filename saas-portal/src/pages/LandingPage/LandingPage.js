8ceaf3a1cfdd1f62cedcdbbbf23b13e4d0056473
Akshat Prakash <akshatprakash@Akshats-MacBook-Pro.local>
2021-04-27 21:16:20 -0400

    added all components, copy and stylistic edits left

--- saas-portal/src/pages/LandingPage/LandingPage.js
+++ saas-portal/src/pages/LandingPage/LandingPage.js
@@ -3,7 +3,7 @@ import React from "react";
 // components
 import LandingNavbar from "../../components/LandingNavbar/LandingNavbar"
 import Hero from "../../components/Hero/Hero"
-import InternCTA from "../../components/InternCTA/InternCTA";
+import InternCTA from "../../components/CTA/InternCTA";
 import InternHowItWorks from "../../components/InternHowItWorks/InternHowItWorks";
 import Stat from "../../components/Stat/Stat";
 import InternFAQs from "../../components/InternFAQs/InternFAQs";
--- saas-portal/src/pages/StartupLandingPage/StartupLandingPage.js
+++ saas-portal/src/pages/StartupLandingPage/StartupLandingPage.js
@@ -5,12 +5,20 @@ import StartupLandingNavbar from "../../components/LandingNavbar/StartupLandingN
 import InternFAQs from "../../components/InternFAQs/InternFAQs";
 import Footer from "../../components/Footer/Footer";
 import StartupHero from "../../components/Hero/StartupHero";
+import StartupPricing from "../../components/Pricing/StartupPricing";
+import StartupCTA from "../../components/CTA/StartupCTA";
+import StartupTestimonials from "../../components/Testimonials/StartupTestimonials";
+import StartupHowItWorks from "../../components/HowItWorks/StartupHowItWorks";
 
 function StartupLandingPage() {
     return (
         <>  
             <StartupLandingNavbar/>
             <StartupHero />
+            <StartupCTA />
+            <StartupHowItWorks />
+            <StartupTestimonials />
+            <StartupPricing />
             <InternFAQs />
             <Footer />
         </>
