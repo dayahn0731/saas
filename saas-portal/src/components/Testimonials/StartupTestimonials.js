b09758467ebf496e52ce23791e5eccec15763b7a
Isabelle Ahn <dayahn0731@gmail.com>
2021-05-24 22:06:22 -0400

    fixed carousel showing all slides

--- saas-portal/src/components/Testimonials/StartupTestimonials.js
+++ saas-portal/src/components/Testimonials/StartupTestimonials.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext } from "pure-react-carousel";
+import 'pure-react-carousel/dist/react-carousel.es.css';
 
 function StartupTestimonials() {
     return (
